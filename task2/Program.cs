4cdb25200dc01bac6514e9d50d80908a55928022
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-03-28 04:09:45 +0300

    add task2 поиск строки с наименьшей суммой элементов

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,12 +1,11 @@
-﻿//Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿//Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 //Например, задан массив:
 //1 4 7 2
 //5 9 2 3
 //8 4 2 4
-//В итоге получается вот такой массив:
-//7 4 2 1
-//9 5 3 2
-//8 4 4 2
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
 Console.Clear();
 Console.Write("Введите количество строк массива: ");
 int rows = int.Parse(Console.ReadLine());
@@ -43,25 +42,24 @@ Random(rows,columns);
 Console.WriteLine("Исходный массив: ");
 PrintArray(Array);
 
-void sortirovka(int[,] array)
+int SumLine(int[,] array, int i)
 {
-for (int i = 0; i < array.GetLength(0); i++)
+int sum = array[i,0];
+for (int j = 1; j < array.GetLength(1); j++)
 {
-for (int j = 0; j < array.GetLength(1) - 1; j++)
+sum += array[i,j];
+}
+return sum;
+}
+
+int minSum = 1;
+int sum = SumLine(Array, 0);
+for (int i = 1; i < Array.GetLength(0); i++)
 {
-for (int k = 0; k < array.GetLength(1) - 1 ; k++)
+if (sum > SumLine(Array, i))
 {
-if (array[i, k] < array[i, k + 1])
-{
-int temp = 0;
-temp = array[i, k + 1];
-array[i, k + 1] = array[i, k];
-array[i, k] = temp;
+sum = SumLine(Array, i);
+minSum = i+1;
 }
 }
-}
-}
-}
-sortirovka(Array);
-Console.WriteLine("Отсортированный массив: ");
-PrintArray(Array);
+Console.WriteLine($"\n Строка c наименьшей суммой элементов: {minSum}");
